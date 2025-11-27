68b3483720efbccf95a2a1e51aed0b6ffabcb3da
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-27 13:27:31 +0545

    int, string, array,slice and print

---
+++ 0.0001/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("i like Cat")
+	a := 1
+	b := 2
+	c := a + b
+
+	fmt.Println(a, b, c)
+	cat := "a domestic animal"
+	bol := true
+	rat := "eat it"
+	fmt.Println(cat, rat, bol)
+
+	// array
+
+	arr := [5]int{1, 2, 3, 4, 5}
+	anotherArray := [2]string{"cat", "rat"}
+	fmt.Println(arr, anotherArray, anotherArray[0], anotherArray[1], arr[4])
+
+	// Slice
+	// thay are like brothers ot array, but more opean minded xd..
+
+	sli := []int{1, 2, 3}
+	fmt.Println(sli)
+	sli = append(sli, 4)
+	fmt.Println(sli)
+	sli = append(sli, 4)
+	fmt.Println(sli)
+	NewSlice := sli[1:3]
+	fmt.Println(NewSlice)
+	// this is somewhat familiar to Python ...
+
+	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	printSlice(s)
+	ss := s[:0]
+	printSlice(ss)
+
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v \n", len(s), cap(s), s)
+}
