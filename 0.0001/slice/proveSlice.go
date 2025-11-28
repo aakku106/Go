809cdf0c5221b10bc8a80b0b8c8ee7f1cadb0594
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-28 18:31:04 +0545

    Deeping more dive in slice

--- 0.0001/slice/proveSlice.go
+++ 0.0001/slice/proveSlice.go
@@ -24,9 +24,50 @@ func proveSlice() {
 	arr = append(arr, "Fat") // Capacity is still 8 and length gets 6
 	printSlic(arr)
 	// so, Go simplly doubles the size of array or slice when its capacity gets filled,
-	// Again slice is a array with fixed capacity, go just doubles the size of array
-}
+	// Again slice is a array with fixed capacity, go just doubles the size of array when capacity is full
+	// When capicity id slice fills up and we append more elements, Go does: old capacity x 2 (oldCapacity*2)
+	newArr := arr[:]
+	printSlic(newArr)
+	newArr[5] = "Super Fat"
+	printSlic(newArr)
+	printSlic(arr)
+	// See, what we did was just changed 6th element or 5th index of slice newArr,
+	// which chaged the value of newarr to [Cats Eats Rats and Becomes Super Fat] from  [Cats Eats Rats and Becomes Fat]
+	// But also 6tm element of arr also changed to [Cats Eats Rats and Becomes Super Fat] from [Cats Eats Rats and Becomes Fat]
+	// This why i told slice of slice [1:]of [1:] is [2:] of slice string
 
+	// One more thong to know about slice, althow the capacity of new array is 8, we cant do
+	newArr[7] = "last index"
+	// we will get runtime error like:
+	//panic: runtime error: index out of range [7] with length 6
+	// Do you know why is this ?
+	// I herd it makes go safe
+	// LEst explore this too
+	/*
+
+				In Go:
+					Length (len) = how many elements your slice currently has.
+					Capacity (cap) = how many elements the underlying array can possibly hold before resizing.
+		- Slices only allow access from index 0 to len-1.
+					Even if capacity is bigger, you can’t use those extra slots directly.
+
+				those extra slots are not part of the slice’s length.
+				They exist in memory, but the slice doesn’t “own” them in its metadata.
+	*/
+	// simplly this means we cannot access elements out of length, so we can say our length is the size of array,
+	// and capacity indicates max length of that array(array not slice because slice simplly points to array in memory adn arrays has fix size, capacity denotes the size of that array, and length denotes size of slice you using. So you can't change the value of underlying array until length of slice reach to that index).
+
+	// Capacity shows potential.
+	// Length shows what’s currently real.
+	// You can NEVER index beyond length.
+	// You CAN append up to capacity before Go reallocates.
+
+	// This much of depth is oki for our biginner level.
+
+}
 func printSlic(s []string) {
 	fmt.Printf("\ngiven Array:\t%v\tLength:\t%d\tCapacity\t%d\n", s, len(s), cap(s))
 }
+
+// This program ends with an runtime error,Try solving this error.
+// Next go to error/main.go file, we will learn error handeling in Go in this same code base
