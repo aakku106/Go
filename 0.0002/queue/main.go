bec9b87632215b54db94f0f2462943f35e034cb8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-10 23:30:28 +0545

    added while loop

--- 0.0002/queue/main.go
+++ 0.0002/queue/main.go
@@ -14,18 +14,23 @@ const (
 )
 
 func main() {
-	fmt.Println("Welci to queue, choose between")
-	fmt.Println("1. Linear Queue")
-	fmt.Println("2. Circular queue")
-	fmt.Println("3. Prority queue")
-	var choose int8
-	switch choose {
-	case quit:
-	case linear:
-		linearQueue()
-	case circular:
-	//	circularQueue()
-	default:
-		fmt.Println("Choose Between 1,2,3 or choose 0 to exit")
+	for {
+
+		fmt.Println("Welci to queue, choose between")
+		fmt.Println("1. Linear Queue")
+		fmt.Println("2. Circular queue")
+		fmt.Println("3. Prority queue")
+		var choose int8
+		fmt.Scan(&choose)
+		switch choose {
+		case quit:
+			return
+		case linear:
+			linearQueue()
+		case circular:
+		//	circularQueue()
+		default:
+			fmt.Println("Choose Between 1,2,3 or choose 0 to exit")
+		}
 	}
 }
