24b7c941b204d7b543c5a749d37fbca32ffb52c7
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-10 23:46:23 +0545

    emqueue, deque and peek function

--- 0.0002/queue/linearQueue.go
+++ 0.0002/queue/linearQueue.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 const (
 	enque = iota + 1
@@ -15,7 +18,10 @@ func linearQueue() {
 	var choose int8
 	fmt.Scan(&choose)
 	switch choose {
-
+	case quit:
+		os.Exit(0)
+	case enque:
+		fmt.Printf("--->\t%d\tInserted in Linear Queue", EnqueIntoLinerQueue())
 	}
 }
 
@@ -23,17 +29,23 @@ func EnqueIntoLinerQueue() int {
 	fmt.Println("Enter value to enqueue")
 	var valueToEnqueueInLinearQueuesTop int
 	fmt.Scan(&valueToEnqueueInLinearQueuesTop)
+	linearQueueStoragePlace = append(linearQueueStoragePlace, valueToEnqueueInLinearQueuesTop)
 	return valueToEnqueueInLinearQueuesTop
 }
 func DequFromLinerQueue() (int, bool) {
 	if isLinearQueueEmpty() {
 		return 0, false
 	}
-	return 1, true
+	dequedValue := linearQueueStoragePlace[0]
+	linearQueueStoragePlace = linearQueueStoragePlace[1:]
+	return dequedValue, true
 }
 func isLinearQueueEmpty() bool {
 	if len(linearQueueStoragePlace) <= 0 {
 		return true
 	}
 	return false
+}
+func PeekIntoLinearQueue() {
+	fmt.Printf("----->\t%d\t<---- is next to come out", linearQueueStoragePlace[0])
 }
