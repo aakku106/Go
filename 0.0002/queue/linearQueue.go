5b69be33b91121eeece121347e25485fdb4801ec
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-10 23:15:24 +0545

    initial functional structure for linear queue

---
+++ 0.0002/queue/linearQueue.go
@@ -0,0 +1,20 @@
+package main
+
+const (
+	enque = iota + 1
+	deque
+	peek
+)
+
+func linearQueue() {
+
+}
+
+func EnqueIntoLinerQueue() {}
+func DequFromLinerQueue()  {}
+func isLinearQueueEmpty() bool {
+	if len(linearQueueStoragePlace) <= 0 {
+		return true
+	}
+	return false
+}
