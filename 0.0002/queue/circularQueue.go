9c25c18abae47da8edbe8872d36008ab64519f9f
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-11 00:33:34 +0545

    initial structure for circular queue

---
+++ 0.0002/queue/circularQueue.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var frontOFcirularQueue, rearOfCircularQueue int = 0, 0
+
+func circularQueue() {
+	fmt.Println("Accessing Circular queue....")
+	fmt.Println("Circular queue accesed")
+	for {
+		fmt.Printf("choose between:\n1.\tEnque\n2.\tDeque\n3.\tPeek\n")
+		var choose int8
+		fmt.Scan(&choose)
+		switch choose {
+		case quit:
+			os.Exit(0)
+		case enque:
+			fmt.Printf("--->\t%d\tInserted in Circular Queue\n", EnqueIntoLinerQueue())
+		case deque:
+			dequedValueFromCircularQueue, wasCircularQueueEmpty := DequFromLinerQueue()
+			if wasCircularQueueEmpty {
+				fmt.Println("Linear Queue was empty...")
+			} else {
+				fmt.Printf("---->\t%d\t<---- Dequed", dequedValueFromCircularQueue)
+			}
+		case peek:
+			PeekIntoCircularQueue()
+		default:
+			fmt.Println("Choose betn 1,2,3 or choose 0 to exit program")
+		}
+	}
+}
+
+func EnqueueInCircularQueue() int {
+	return 1
+}
+
+func PeekIntoCircularQueue() {
+	if isCircularQueueEmpty() {
+		fmt.Println("The Circular Queue is empty")
+	} else {
+		fmt.Printf("---->%d<----will come out next...\n", 1)
+	}
+}
+func isCircularQueueEmpty() bool {
+	if len(circularQueueStoragePlace) <= 0 {
+		return true
+	}
+	return false
+}
