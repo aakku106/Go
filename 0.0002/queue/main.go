5a0356bdd3a46405de6c5014b43a3cbac8c38e0b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-10 23:15:42 +0545

    main function with simple switch case

---
+++ 0.0002/queue/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+var linearQueueStoragePlace []int
+
+const (
+	quit = iota
+	linear
+	circular
+	prority
+)
+
+func main() {
+	fmt.Println("Welci to queue, choose between")
+	fmt.Println("1. Linear Queue")
+	fmt.Println("2. Circular queue")
+	fmt.Println("3. Prority queue")
+	var choose int8
+	switch choose {
+	case quit:
+	case linear:
+		linearQueue()
+	case circular:
+	//	circularQueue()
+	default:
+		fmt.Println("Choose Between 1,2,3 or choose 0 to exit")
+	}
+}
