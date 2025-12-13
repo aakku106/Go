276a2c8074b21c599870f6458dc7d61d1aa52d06
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-13 12:54:26 +0545

    added a comment about: plannign hwo to implement the prority queue

--- 0.0002/queue/prorityQueue.go
+++ 0.0002/queue/prorityQueue.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var (
+	prorityValue int
+)
+
 func prorityQueue() {
+	fmt.Println("Accessing Protity Queue.....")
+	fmt.Println("Protuty Queue Accesed")
 
 }
+
+/*
+Lest see how we make  prority queue:
+1. 1st it shall follow fifo rele: first in 1st out,which means i have to 1st make a normal linear queue(not circular queue cause i have a idea to use slice like a ummm some shord of garbage collector thing like, when lest say slice is larger than 50, ie. the front is larger than 50 than only do [front:], and front=0; i think it should be ~ O(1)may be lest cee)
+2. i need a prority, so lest decleare a var called prority, where we can give values from 0 to 4, lower the value higher the prority, I think this is good idea for now
+*/
