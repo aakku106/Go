e3de54000326cb0a474fb020eb5ad4f690c5b1eb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-13 14:45:26 +0545

    2 values input one for value another for prority

--- 0.0002/queue/prorityQueue.go
+++ 0.0002/queue/prorityQueue.go
@@ -33,6 +33,35 @@ func prorityQueue() {
 	}
 }
 func EnqueueInProrityQueue() (int, int) {
+	fmt.Println("Enter the value to Enqueue in Prority Queue")
+	fmt.Println("In format: value,prority")
+	fmt.Println("Note: Prority shall be from 0 to 4, lower the value, higher the prority")
+
+	fmt.Printf("\tlen:\t%d\tcap:\t%d\n", len(prorityQueueStoragePlace), cap(prorityQueueStoragePlace))
+	// fmt.Printf("\tlen of %d:\t%d\tcap of %d:\t%d\n",
+	//	len(prorityQueueStoragePlace[0][0]), cap(prorityQueueStoragePlace[0][0]))
+
+	var (
+		value   int
+		prority int8
+	)
+	fmt.Scanf("%d,%d", &value, &prority)
+	switch prority {
+	case 0:
+		prorityQueueStoragePlace[0] = append(prorityQueueStoragePlace[0], value)
+	case 1:
+		prorityQueueStoragePlace[1] = append(prorityQueueStoragePlace[1], value)
+	case 2:
+		prorityQueueStoragePlace[2] = append(prorityQueueStoragePlace[2], value)
+	case 3:
+		prorityQueueStoragePlace[3] = append(prorityQueueStoragePlace[3], value)
+	case 4:
+		prorityQueueStoragePlace[4] = append(prorityQueueStoragePlace[4], value)
+	default:
+		fmt.Println("Cloose between: 0,1,2,3,4")
+	}
+	fmt.Printf("\n%v\n", prorityQueueStoragePlace)
+
 	return 1, 1
 }
 func peekIntoProrityQueue() {
