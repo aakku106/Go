9c212e3446778e75bf47c0db4946f159951830f0
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-18 14:21:25 +0545

    ru ti ep olymerphism

--- 0.0008/interface/main.go
+++ 0.0008/interface/main.go
@@ -24,7 +24,26 @@ type square struct {
 
 func main() {
 	newRect := rectangle{height: 12.32, width: 32.12}
+	newCircle := circle{radious: 19.43}
+	newTraingle := traingle{height: 42.32, base: 341.4221}
+	newSquare := square{length: 1221.32221}
 
+	// Normal just like before
+	fmt.Println(
+		newCircle.area(),
+		newSquare.area(),
+		newRect.area(),
+		newTraingle.area(),
+	) // Well we can this to be consider this as polymorphism
+
+	// We can use interface
+	areaCalc(&newRect)
+	areaCalc(&newCircle)
+	areaCalc(&newSquare)
+	areaCalc(&newTraingle)
+	// but these are runtime polymorphism
+	// dosent this somewhat feels like working with classes, but different
+	// the & before struct name is because I used pointer of struct in method defination
 }
 
 func (r *rectangle) area() float64 { return r.height * r.width }
