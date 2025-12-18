c3319bc060a632fdae435281c396598ff16878c8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-18 09:06:50 +0545

    defining struct

--- 0.0008/interface/main.go
+++ 0.0008/interface/main.go
@@ -6,6 +6,19 @@ type Shape interface {
 	area() float64
 }
 
+type rectangle struct {
+	height, width float64
+}
+type circle struct {
+	radious float64
+}
+type traingle struct {
+	height, base float64
+}
+type square struct {
+	length float64
+}
+
 func main() {
 
 }
