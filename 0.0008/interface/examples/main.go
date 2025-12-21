1293eac670ec7614fed531bbda61943eb05ffc1a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-21 09:40:15 +0545

    playing with empty interface

--- 0.0008/interface/examples/main.go
+++ 0.0008/interface/examples/main.go
@@ -51,4 +51,41 @@ func emptyInterface() {
 				Go: here wo do strong type safty at compile time to avoid wired behavior of code
 		meanwhile users: Lest turn go into Js by using any everywhere
 	*/
+	domesticAnimals := map[cat]cat{
+		1:     "cat",
+		"dog": 2,
+		// We breaked every rule of Go
+		// May be useful in JSON
+	}
+	printEmtyInterfacetypes(domesticAnimals)
+	// lest test array/slice
+	var something cat
+	something = []cat{1, 2, "cat"}
+	printEmtyInterfacetypes(something)
+	// we can also do
+	something = map[any]cat{
+		"cat": 1,
+		2:     "bat",
+		true:  false,
+		// why i used any here, why not ?xd
+	}
+	printEmtyInterfacetypes(something)
+	// Lest look at more fun things
+	emptyInterfaceVariadicType(fmt.Println())
+	emptyInterfaceVariadicType(fmt.Println("buhuhahaha"))
+	emptyInterfaceVariadicType(1, 2, 3)
+	emptyInterfaceVariadicType(fmt.Println("cat"))
+	emptyInterfaceVariadicType(fmt.Print("cat"))
+	emptyInterfaceVariadicType(fmt.Printf("cat"))
+	emptyInterfaceVariadicType(fmt.Printf(""))
+	emptyInterfaceVariadicType(fmt.Print(""))
+	emptyInterfaceVariadicType(fmt.Print())
+	// basicallly ...cat makes a array of cat type, simple
+
+}
+func printEmtyInterfacetypes(v cat) {
+	fmt.Println(v)
+}
+func emptyInterfaceVariadicType(v ...cat) {
+	fmt.Println(v)
 }
