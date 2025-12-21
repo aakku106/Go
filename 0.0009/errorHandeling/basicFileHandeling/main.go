bb4a5d60b04ea29dd56e077d0bdf437da9e02586
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-21 16:34:31 +0545

    reading a file

--- 0.0009/errorHandeling/basicFileHandeling/main.go
+++ 0.0009/errorHandeling/basicFileHandeling/main.go
@@ -18,11 +18,19 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+	defer file.Close()
 	fmt.Println(file, " cause car.txt dont exist till now in this dir")
 	file, err = os.Open("cat.txt")
 	if err != nil {
 		log.Println(err)
 	}
+	defer file.Close()
 	fmt.Println(file, " shows memory value cause cat.txt exists")
 	fmt.Println(file.Name())
+	fmt.Println(file.WriteString("awwwwwwwwwwwwwwwwwwwwww"))
+	fileRead, err := os.ReadFile("fat.txt")
+	if err != nil {
+		log.Println("It sems thers no file with name: fat.txt")
+	}
+	fmt.Println(string(fileRead))
 }
