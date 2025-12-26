51d613e9aa5e818d18ff09abdaf52d462aaaae12
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-26 23:18:45 +0545

    proper naming of testing function and commenting tip retine fujnctoin call

--- 0.0012/concurrency/test/concurrency_test.go
+++ 0.0012/concurrency/test/concurrency_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 )
 
-func TestConc(t *testing.T) {
+func TestGoRutines(t *testing.T) {
 	// concurrency.WaitToGetGet()
 	concurrency.C1()
 	// we won't see any output from go get function of C1, function (but those get function will be printed because we waited for some seconds in WaitToGetGet functer below, thats the resion all go get from c1 wont be printed cause timwA we waited weren't enougth to print them all)
 	concurrency.WaitToGetGet()
 	// if you call  concurrency.WaitToGetGet() before concurrency.C1() you can observe only wee will be printed
+
+	// that's basic demosestration of go rutines
 }
