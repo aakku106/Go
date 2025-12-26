9496740cb68fa0be819987329cefcfb1b3e2fcae
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-26 23:14:22 +0545

    logical buildign about how concurency work in go

--- 0.0012/concurrency/channels.go
+++ 0.0012/concurrency/channels.go
@@ -1,6 +1,9 @@
 package concurrency
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func C1() {
 	fmt.Println("Welcom master.....")
@@ -23,14 +26,21 @@ func C1() {
 		what happened here is go forked those get function we called in thoir one sebthreads
 		its normally called asynchronous behavior
 		main func will continue doing its job and won't wait for those job to be done to move on to next job
-		ie, whenever we put go infront of any function the parent function won't wait that function to do its job and continue doing its job ignoring that function
+		ie, whenever we put go infront of any function the parent function won't wait
+		that function to do its job and continue doing its job ignoring that function
 
 		In our case, main function just did that it continued doing its job and ignored job of go get(...)
-		and when main function dies everything dies, althow we dont have any main function our code, the behavior will be same when C1 goes out of stact all get will also go if their job are not don
-		what can we do to solve this ?
+		and when main function dies everything dies, althow we dont have any main function our code,
+		the behavior will be same when our program(main thread) dies it kills all other thereds/subthereds/hyperthreads with it
+		so go get(...) output were not able to come until the main thread dies,so we saw no putput from go get()
+		what can we do to solve this ? well most easy way is to wait for them
 	*/
 
 }
 func get(s string) {
 	fmt.Println("got: ", s)
+}
+func WaitToGetGet() {
+	go get("weeeeee")
+	time.Sleep(time.Second)
 }
--- 0.0012/concurrency/test/concurrency_test.go
+++ 0.0012/concurrency/test/concurrency_test.go
@@ -2,11 +2,13 @@ package test
 
 import (
 	"concurrency"
-	"fmt"
 	"testing"
 )
 
 func TestConc(t *testing.T) {
-	fmt.Println("weeee")
+	// concurrency.WaitToGetGet()
 	concurrency.C1()
+	// we won't see any output from go get function of C1, function (but those get function will be printed because we waited for some seconds in WaitToGetGet functer below, thats the resion all go get from c1 wont be printed cause timwA we waited weren't enougth to print them all)
+	concurrency.WaitToGetGet()
+	// if you call  concurrency.WaitToGetGet() before concurrency.C1() you can observe only wee will be printed
 }
