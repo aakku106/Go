ff7a90f4dd3ed86945e8a9b45f31390f723b1400
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-27 22:13:12 +0545

    un-buffered example 1
    Send waits until receive happens condition

--- 0.0012/concurrency/channels.go
+++ 0.0012/concurrency/channels.go
@@ -19,5 +19,12 @@ import "fmt"
 
 // Lest look at un-Buffereed channel 1st in fucn T1
 func T1() {
+	ch := make(chan int)
+	// Here we created a pipe name ch which can carry(only) int type
+	go func() { ch <- 10 }()
+	x := <-ch
+	fmt.Println(x)
+	// you might think what are those wired symboles and wired code writtem, than continue reading
+}
 
 }
