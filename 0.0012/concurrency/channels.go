c4eddd2ae4efe8d5f47d3ee846a42fd283987208
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-27 22:12:13 +0545

    simple explanation of channels in go

--- 0.0012/concurrency/channels.go
+++ 0.0012/concurrency/channels.go
@@ -2,6 +2,11 @@ package concurrency
 
 import "fmt"
 
+/*
+## Channels in Go
+- simpally talking channel are like a pipe
+	put somethign in 1 side, and get that thing out from other side
+
 // There are generally 2 types oc channels in Go
 //	i.	Un-Buffered channel
 //	ii. Buffered Channel
