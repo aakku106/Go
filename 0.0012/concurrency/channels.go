5953637c34e345e5b725f4c7a235a42c5806ef15
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-27 22:12:37 +0545

    Supper simple rules of channel in go
    its basic lvl

--- 0.0012/concurrency/channels.go
+++ 0.0012/concurrency/channels.go
@@ -7,6 +7,12 @@ import "fmt"
 - simpally talking channel are like a pipe
 	put somethign in 1 side, and get that thing out from other side
 
+### RULE
+- Send wait untill Receive happens,
+- Receive waits until Send happens
+? sounds confusing, you are in right path, continue reading
+*/
+
 // There are generally 2 types oc channels in Go
 //	i.	Un-Buffered channel
 //	ii. Buffered Channel
