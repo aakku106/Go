0d26aff972082425454e4c283f9cf46b0fe0c6b6
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-27 21:53:27 +0545

    moved comments in their proper place

--- 0.0012/concurrency/channels.go
+++ 0.0012/concurrency/channels.go
@@ -2,11 +2,11 @@ package concurrency
 
 import "fmt"
 
-func T1() {
-	// There are generally 2 types oc channels in Go
-	//	i.	Un-Buffered channel
-	//	ii. Buffered Channel
+// There are generally 2 types oc channels in Go
+//	i.	Un-Buffered channel
+//	ii. Buffered Channel
 
-	// Lest look at un-Buffereed channel 1st in fucn T1
+// Lest look at un-Buffereed channel 1st in fucn T1
+func T1() {
 
 }
