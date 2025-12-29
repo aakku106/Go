fdc5f3bf7734a56a82becb2604612ba6a47cae97
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-12-29 14:35:10 +0545

    select initial structure

--- 0.0012/concurrency/select.go
+++ 0.0012/concurrency/select.go
@@ -1,1 +1,30 @@
 package concurrency
+
+// in go select somewhat looks like switch-case (syntex)
+
+func S1() {
+	ch := make(chan int)
+	c := make(chan string)
+
+	go func() {
+		ch <- 106
+	}()
+	go func() {
+		c <- "weeeeee"
+	}()
+}
+
+// We already look at this behavior, 1 goutine reaches ch<-106 and waits there infinite and another go rutine
+// infinetly waits at c<- ... point and receive never happen, in mean time main dyes and every gorutine was killed, no one noticed
+// Lest look at exmple of select(how we catch/receive)
+func S2() {
+	ch := make(chan int)
+	c := make(chan string)
+
+	go func() {
+		ch <- 106
+	}()
+	go func() {
+		c <- "weeeeee"
+	}()
+}
