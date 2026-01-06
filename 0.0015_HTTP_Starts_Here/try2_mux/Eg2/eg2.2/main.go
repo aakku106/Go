d3a82657bb7c0007d2a824633776b2ebb1f2ffd8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-06 16:09:04 +0545

    Server listening in port 80

--- 0.0015_HTTP_Starts_Here/try2_mux/Eg2/eg2.2/main.go
+++ 0.0015_HTTP_Starts_Here/try2_mux/Eg2/eg2.2/main.go
@@ -4,4 +4,28 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("/cat", handleCat)
+	mux.HandleFunc("/dog", handleDog)
+
+	server := http.Server{}
+	server.Handler = mux
+	server.ListenAndServe()
+
 }
+func handleCat(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Only cat can come here, but Welcome Master CCN"))
+}
+
+func handleDog(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Only Dog can come here, but Welcome Master CCN"))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is root Welcome Master CCN"))
+}
+
+/*
+Now this server is listening in port 80 or simpally localhost it self
+*/
