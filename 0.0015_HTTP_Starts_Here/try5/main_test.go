c57c442107a115820db54c74f364444016289b37
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-12 00:42:53 +0545

    Purpose of test file

--- 0.0015_HTTP_Starts_Here/try5/main_test.go
+++ 0.0015_HTTP_Starts_Here/try5/main_test.go
@@ -2,6 +2,13 @@ package try5
 
 import "testing"
 
+/*
+This entire testing file is useless and un-profectional,
+But than Why did i made one and using it
+Because Go test automatically shutdown/fail in around ~600 sec.
+I use TMUX and MAC so i never close my mac and tmux never kills my servers so i
+intensionally made this test fiel which autokills itself in ~600 sec its saves my CPU,Memory and most imp battery
+*/
 func TestDummy(t *testing.T) {
 	InitilizeServer()
 }
