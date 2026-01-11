cfc322e78d91b9daf700b101e86da040206641a4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-11 23:00:27 +0545

    Redirecting

---
+++ 0.0015_HTTP_Starts_Here/try5/main.go
@@ -0,0 +1,31 @@
+package try5
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func InitilizeServer() {
+
+	fmt.Println("Starting The server in port 8080")
+
+	mux := http.NewServeMux()
+	server := http.Server{}
+
+	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("/portfolio", handlePortfolio)
+	mux.HandleFunc("/contact", handleContact)
+
+	server.Addr = ":8080"
+	server.Handler = mux
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Println("Error: ", err)
+	}
+
+}
+
+func handleRoot(writer http.ResponseWriter, request *http.Request)      {}
+func handlePortfolio(writer http.ResponseWriter, request *http.Request) {}
+func handleContact(writer http.ResponseWriter, request *http.Request)   {}
