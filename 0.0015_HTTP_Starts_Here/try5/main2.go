90fae10e7a82fdc45cbb4e86d132482d01d24ce9
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-12 13:17:57 +0545

    Explenation

--- 0.0015_HTTP_Starts_Here/try5/main2.go
+++ 0.0015_HTTP_Starts_Here/try5/main2.go
@@ -40,3 +40,28 @@ func handleRoot2(writer http.ResponseWriter, request *http.Request) {
 	}
 	fmt.Println("Body content:", string(bodyBytes))
 }
+
+/*
+Here whenever we do
+
+❯ curl -X POST http://localhost:8080/contact \                     ─╯
+     -H "Content-Type: application/json" \
+     -d '{"email":"aakku@gmail.com","msg":"hello"}'
+
+we get (for user)
+
+Only Get here
+weeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaa%
+
+for server
+
+Starting The server in port 8080
+Body: &{0x1400000e210 <nil> <nil> false true {{} {0 0}} false false false 0x100992160}
+Body: &{0x1400000e210 <nil> <nil> false true {{} {0 0}} false false false 0x100992160}
+Body content: {"email":"aakku@gmail.com","msg":"hello"}
+
+Althow we had if condition to filter out only GET, but since we dont have return in that if block
+whatever below will strill run and response (Could be dengerious in producton, this is just to show/test)
+*/
+
+//NEXT: ./main3.go we will look into URL into deep
