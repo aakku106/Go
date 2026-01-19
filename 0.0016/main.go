3f039775ab9ca040d66b136ea894d9b66a4919c4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-19 22:13:00 +0545

    Func to move around and Teleport

--- 0.0016/main.go
+++ 0.0016/main.go
@@ -15,4 +15,21 @@ type Enemy struct {
 	*Position
 }
 
-func main() {}
+func (p *Position) MoveRight() {
+	p.x += 2
+}
+func (p *Position) MoveLeft() {
+	p.x -= 2
+}
+func (p *Position) MoveUp() {
+	p.y += 2
+}
+func (p *Position) MoveDown() {
+	p.y -= 2
+}
+func (p *Position) Teleport(x, y float64) {
+	p.x = x
+	p.y = y
+}
+
+func NewPlayer() *Player {
