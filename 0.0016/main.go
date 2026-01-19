1f0be28fe3b6386c5dd6c751a15df3925a6b493b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-19 23:42:54 +0545

    Add game module and movement functions for player control

--- 0.0016/main.go
+++ 0.0016/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"image/color"
+	"log"
 )
+
+type Game struct{}
 
 type Position struct {
 	x float64
@@ -15,31 +21,53 @@ type Enemy struct {
 	*Position
 }
 
-func (p *Position) MoveRight() {
-	p.x += 2
-}
-func (p *Position) MoveLeft() {
-	p.x -= 2
-}
-func (p *Position) MoveUp() {
-	p.y += 2
-}
-func (p *Position) MoveDown() {
-	p.y -= 2
-}
-func (p *Position) Teleport(x, y float64) {
-	p.x = x
-	p.y = y
-}
-
 func NewPlayer() *Player {
 	return &Player{
 		Position: &Position{},
 	}
 }
+
+var aakku = NewPlayer()
+
+func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		aakku.MoveUp()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		aakku.MoveDown()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		aakku.MoveLeft()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		aakku.MoveRight()
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0, 0, 0, 255}) // clear screen to black
+
+	var path vector.Path
+	path.MoveTo(float32(aakku.Position.x), float32(aakku.Position.y))
+	path.ArcTo(float32(aakku.Position.x), float32(aakku.Position.y), 20, 0, 2*3.14159) // full circle
+	path.Close()
+
+	op := &ebiten.DrawTrianglesOptions{}
+	vector.Fill(screen, path, color.RGBA{255, 0, 0, 255}, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	// Size of the logical screen.
+	return 640, 480
+}
+
 func main() {
-	aakku := NewPlayer()
-	aakku.MoveUp()
-	aakku.MoveLeft()
-	fmt.Println(aakku.Position)
+	game := &Game{}
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Weeeee")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
