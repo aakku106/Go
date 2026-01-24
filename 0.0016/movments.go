5bc490e098e5ff26f2678c176183cdd8bd6ac1d0
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-24 21:04:24 +0545

    Fixing movments

--- 0.0016/movments.go
+++ 0.0016/movments.go
@@ -7,10 +7,10 @@ func (p *Position) MoveLeft() {
 	p.x -= 2
 }
 func (p *Position) MoveUp() {
-	p.y += 2
+	p.y -= 2
 }
 func (p *Position) MoveDown() {
-	p.y -= 2
+	p.y += 2
 }
 func (p *Position) Teleport(x, y float64) {
 	p.x = x
