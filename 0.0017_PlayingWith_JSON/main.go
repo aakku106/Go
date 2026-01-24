40bd38ac699b5787b51eb82eabdcb9246ebb2547
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-24 21:04:47 +0545

    initial structire to play with json

---
+++ 0.0017_PlayingWith_JSON/main.go
@@ -0,0 +1,9 @@
+package playingwithjson
+
+import "encoding/json"
+
+/*
+Go standard libary provides a built in package to work with JSON (	encoding/decoding ) and struct (serilize/deserilize)
+Go has encoding/json package, has toold like marshal(convert Go data to JSON)
+and unmarshal (Converting JSON to Go data)
+*/
