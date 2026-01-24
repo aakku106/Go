8d16299cc68f96203962ffaf6a3554938d1520c5
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-25 00:12:50 +0545

    Refactor test functions for clarity and organization

--- 0.0017_PlayingWith_JSON/main_test.go
+++ 0.0017_PlayingWith_JSON/main_test.go
@@ -2,18 +2,13 @@ package playingwithjson
 
 import "testing"
 
-func Test_call1(t *testing.T) {
+func est_call(t *testing.T) {
 	call1()
-}
-func Test_call2(t *testing.T) {
 	call2()
-}
-func Test_call3(t *testing.T) {
 	call3()
-}
-func Test_call4(t *testing.T) {
 	call4()
-}
-func Test_call5(t *testing.T) {
 	call5()
+}
+func Test_um(t *testing.T) {
+	um2()
 }
