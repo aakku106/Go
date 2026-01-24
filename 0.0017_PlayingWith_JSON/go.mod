3ea1884ff6b762f0258054cd9ea2280f30ec4032
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-24 22:17:16 +0545

    mod & rtest

---
+++ 0.0017_PlayingWith_JSON/go.mod
@@ -0,0 +1,3 @@
+module playingWithJSON
+
+go 1.25.6
---
+++ 0.0017_PlayingWith_JSON/main_test.go
@@ -0,0 +1,19 @@
+package playingwithjson
+
+import "testing"
+
+func Test_call1(t *testing.T) {
+	call1()
+}
+func Test_call2(t *testing.T) {
+	call2()
+}
+func Test_call3(t *testing.T) {
+	call3()
+}
+func Test_call4(t *testing.T) {
+	call4()
+}
+func Test_call5(t *testing.T) {
+	call5()
+}
