2fb17720dd3e211e99c9f9812555fee593875c0a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-02-07 03:40:51 +0545

    playing with db

--- 0.0020_PlayyingWith_DB/main.go
+++ 0.0020_PlayyingWith_DB/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
+	connStr := "postgres://postgres:aakku106@localhost/gofirst?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	createTable(db)
+}
+
+// One shall use migrations for actual use, this is just example
+func createTable(db *sql.DB) {
+
+	query := `create table Users
+	id serial primary key,
+	name varchar(20) not null,
+	address varchar(50) not null
+	`
+
+	log.Println("Creating Table")
+	if _, err := db.Exec(query); err != nil {
+		log.Fatal(err)
+	}
 
 }
