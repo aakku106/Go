336c3e98d74370b4b286a38185346b47266d5c24
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-02-06 23:42:51 +0545

    calling func to write file

--- 0.0019/examples/files_test.go
+++ 0.0019/examples/files_test.go
@@ -6,7 +6,13 @@ import (
 
 func TestFiles(t *testing.T) {
 	err := ReadFile("files.go")
-	if err == nil {
+	if err != nil {
 		t.Fatal("The file ./files.go should exitst, but we get error")
 	}
+
+	err = ReadFile2("files.go")
+	if err != nil {
+		t.Fatal("The file ./files.go should exitst, but we get error")
+	}
+
 }
