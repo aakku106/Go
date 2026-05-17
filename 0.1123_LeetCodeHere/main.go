512fe33878eca198a4574668cbb98e8a293eac58
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-05-18 05:15:20 +0545

    two sum array
    Bholi garni ho yo chai 5 bajay sako aaja bihanako sutako xaina xd

---
+++ 0.1123_LeetCodeHere/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	numbers := []int{2, 4, 6, 8}
+	target := 10
+	twoSum(numbers, target)
+}
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+
+	}
+}
