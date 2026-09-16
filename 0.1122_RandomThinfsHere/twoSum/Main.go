4383cbf76755464dd0f78db1e2777dcef8bc8dbe
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-17 01:57:13 +0545

    optima; soln

--- 0.1122_RandomThinfsHere/twoSum/Main.go
+++ 0.1122_RandomThinfsHere/twoSum/Main.go
@@ -4,8 +4,14 @@ import ()
 
 func twoSum(nums []int, target int) []int {
 
-	// HM := make(map[int]int, len(nums))
-	//
+	HM := make(map[int]int, len(nums))
+	for i := range nums {
+		if j, ok := HM[target-nums[i]]; ok {
+			return []int{j, i}
+		}
+		HM[nums[i]] = i
+	}
+
 	return nil
 }
 
